b1494be008a534a44f7103058f837a995db74833
EinarsNG <>
2022-05-04 02:28:38 +0300

    Seperated parts of code into functions

--- update_monitor.py
+++ update_monitor.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from argparse import ArgumentParser
 
 import platform
 import re
@@ -10,12 +9,11 @@ import sys
 import subprocess
 import shutil
 
-parser = ArgumentParser()
-parser.add_argument("--nowarn", action="store_true", help="Disable package not found warning")
-
 SCRIPT_PATH = os.path.dirname(sys.argv[0])
-
+URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&$\/\/=]*)"
+DEFAULT_REPOS = ["core", "extra", "community"]
 BAR_WIDTH = 50
+
 def progress_bar(current: int | float, max_value: int | float):
     current_progress = current / max_value * BAR_WIDTH
     left_progress = BAR_WIDTH - int(current_progress)
@@ -24,8 +22,11 @@ def progress_bar(current: int | float, max_value: int | float):
     print(f"\r[{done_symbols}{left_symbols}] {current_progress*100/BAR_WIDTH:.2f}%", end="")
 
 def get_current_packages() -> dict:
-    packages = {}
-    process = subprocess.Popen([shutil.which("pacman"), "-Q"], stdout=subprocess.PIPE)
+    packages: dict = {}
+    pacman: str | None = shutil.which("pacman")
+    if not pacman:
+        raise FileNotFoundError("Pacman binary not found")
+    process = subprocess.Popen([pacman, "-Q"], stdout=subprocess.PIPE)
     lines = process.stdout.readlines()
     for line in lines:
         line = line.decode().strip().split(" ")
@@ -34,29 +35,31 @@ def get_current_packages() -> dict:
         packages[name] = version
     return packages
 
-URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&$\/\/=]*)"
-def main() -> None:
-    arch = platform.machine()
+def update_repos() -> list[str]:
+    arch: str = platform.machine()
     try:
-        with open("./repos.txt") as f:
+        with open(os.path.join(SCRIPT_PATH, "repos.txt")) as f:
             repos = [line.strip() for line in f.readlines()]
     except FileNotFoundError:
-        repos = ["core", "extra", "community"]
+        repos = DEFAULT_REPOS
 
     mirror: str = ""
     try:
         with open("/etc/pacman.d/mirrorlist") as f:
             for line in f:
+                # Those servers which are commented out should be ignored
+                if line[0] == "#":
+                    continue
                 if res := re.search(URL_REGEX, line):
                     mirror = res[0]
                     break
     except FileNotFoundError:
-        with open("./mirror.txt") as f:
+        with open(os.path.join(SCRIPT_PATH, "mirror.txt")) as f:
             mirror = f.readline().strip()
 
-    urls_to_query = []
-    repo_files_to_download = []
-    repo_files = []
+    urls_to_query: list[str] = []
+    repo_files_to_download: list[str] = []
+    repo_files: list[str] = []
     time_now = int(datetime.now().timestamp())
     for repo in repos:
         repo_file = f"{repo}.db"
@@ -86,7 +89,10 @@ def main() -> None:
                     f.write(data)
                 print()
 
-    new_packages = {}
+    return repo_files
+
+def get_repo_packages(repo_files: list[str]) -> dict:
+    new_packages: dict = {}
     for repo_file in repo_files:
         try:
             with tarfile.open(os.path.join(SCRIPT_PATH, repo_file), "r") as tar:
@@ -110,19 +116,28 @@ def main() -> None:
         except tarfile.ReadError:
             print(f"Could not open malformed {repo_file}.")
 
+    return new_packages
+
+def get_new_available(current_packages: dict, new_packages: dict) -> list[tuple[str, str, str]]:
+    new_available: list[tuple[str, str, str]] = []
+    for curPackage, curVersion in current_packages.items():
+        for newPackage, newVersion in new_packages.items():
+            if (curPackage == newPackage
+                    and curVersion != newVersion):
+                new_available.append((curPackage, curVersion, newVersion))
+    return new_available
+
+def main() -> None:
+    repo_files = update_repos()
+    new_packages = get_repo_packages(repo_files) 
     current_packages = get_current_packages()
-    new_available = 0
-    for package, version in current_packages.items():
-        if package not in new_packages:
-            if not args.nowarn:
-                print(f"Warning {package} not found")
-            continue
-        if new_packages[package] != version:
-            new_available += 1
-            print(package, version, f"New version available ({new_packages[package]})")
-    if new_available == 0:
+    new_available = get_new_available(current_packages, new_packages)
+    if not new_available:
         print("No new packages available")
+    else:
+        for available in new_available:
+            print(f"{available[0]} - Current: {available[1]} - Newest: {available[2]}")
+        print(f"{len(new_available)} new packages available")
 
 if __name__ == "__main__":
-    args = parser.parse_args()
     main()
