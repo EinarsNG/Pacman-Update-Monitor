51a1cacfb4f855e6b402e4c4d2a13802c94b6812
EinarsNG <>
2022-05-06 22:31:12 +0300

    Implemented email sending

--- update_monitor.py
+++ update_monitor.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from email.message import EmailMessage
+from smtplib import SMTP
 
 import platform
 import re
@@ -8,8 +10,9 @@ import tarfile
 import sys
 import subprocess
 import shutil
+import json
 
-SCRIPT_PATH = os.path.dirname(sys.argv[0])
+SCRIPT_DIR = os.path.dirname(sys.argv[0])
 URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&$\/\/=]*)"
 DEFAULT_REPOS = ["core", "extra", "community"]
 BAR_WIDTH = 50
@@ -38,7 +41,7 @@ def get_current_packages() -> dict:
 def update_repos() -> list[str]:
     arch: str = platform.machine()
     try:
-        with open(os.path.join(SCRIPT_PATH, "repos.txt")) as f:
+        with open(os.path.join(SCRIPT_DIR, "repos.txt")) as f:
             repos = [line.strip() for line in f.readlines()]
     except FileNotFoundError:
         repos = DEFAULT_REPOS
@@ -54,7 +57,7 @@ def update_repos() -> list[str]:
                     mirror = res[0]
                     break
     except FileNotFoundError:
-        with open(os.path.join(SCRIPT_PATH, "mirror.txt")) as f:
+        with open(os.path.join(SCRIPT_DIR, "mirror.txt")) as f:
             mirror = f.readline().strip()
 
     urls_to_query: list[str] = []
@@ -82,7 +85,7 @@ def update_repos() -> list[str]:
             r.raise_for_status()
             total_size = r.headers.get("content-length", 0)
             downloaded_size = 0
-            with open(os.path.join(SCRIPT_PATH, repo_file), "wb") as f:
+            with open(os.path.join(SCRIPT_DIR, repo_file), "wb") as f:
                 for data in r.iter_content(32 * 1024):
                     downloaded_size += len(data)
                     progress_bar(downloaded_size, int(total_size))
@@ -95,7 +98,7 @@ def get_repo_packages(repo_files: list[str]) -> dict:
     new_packages: dict = {}
     for repo_file in repo_files:
         try:
-            with tarfile.open(os.path.join(SCRIPT_PATH, repo_file), "r") as tar:
+            with tarfile.open(os.path.join(SCRIPT_DIR, repo_file), "r") as tar:
                 for member in tar:
                     if member.isfile() and re.match(".*desc$", member.name):
                         name = ""
@@ -127,17 +130,69 @@ def get_new_available(current_packages: dict, new_packages: dict) -> list[tuple[
                 new_available.append((curPackage, curVersion, newVersion))
     return new_available
 
+def send_notification(html_body: str) -> None:
+    with open(os.path.join(SCRIPT_DIR, "email.json")) as f:
+        config = json.load(f)
+    email_msg = EmailMessage()
+    now_time = datetime.now()
+    email_msg["Subject"] = now_time.strftime(config["title_format"])
+    email_msg["From"] = config["sender_email"]
+    email_msg["To"] = config["recipient"]
+    email_msg.add_header("Content-Type", "text/html")
+    email_msg.set_payload(html_body)
+    
+    sender = config["sender_email"].split("@")
+    username = sender[0]
+    password = config["sender_password"]
+    sender_domain = sender[1]
+    with SMTP(host=sender_domain, port=config["email_port"]) as smtp:
+        if config["auth_mechanism"] == "starttls":
+            smtp.starttls()
+        smtp.login(username, password)
+        smtp.send_message(email_msg)
+
+def construct_html(text: str, new_available: list[tuple[str, str, str]]) -> str:
+    if not new_available:
+        return f"<html><body><h3>{text}</h3></body></html>"
+    html_body = f"""
+    <html>
+    <style>table,th,td{{border-collapse: collapse;border: 1px solid black;}}</style>
+    <body>
+    <h3>{text}</h3>
+    <table>
+    <tr>
+        <th>Package name</th>
+        <th>Current version</th>
+        <th>Newest version</th>
+    </tr>
+    """
+    for entry in new_available:
+        html_body += f"""
+        <tr>
+            <td>{entry[0]}</td>
+            <td>{entry[1]}</td>
+            <td>{entry[2]}</td>
+        </tr>
+        """
+    html_body += "</body></table></html>"
+    return html_body
+
 def main() -> None:
     repo_files = update_repos()
     new_packages = get_repo_packages(repo_files) 
     current_packages = get_current_packages()
     new_available = get_new_available(current_packages, new_packages)
+    msg_text = ""
     if not new_available:
-        print("No new packages available")
+        msg_text = "No new packages available"
     else:
         for available in new_available:
             print(f"{available[0]} - Current: {available[1]} - Newest: {available[2]}")
-        print(f"{len(new_available)} new packages available")
+        msg_text = f"{len(new_available)} new packages available"
+    print(msg_text)
+
+    html_body = construct_html(msg_text, new_available)
+    send_notification(html_body)
 
 if __name__ == "__main__":
     main()
